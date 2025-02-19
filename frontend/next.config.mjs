9eb33b9e947301a1cce705fd9246b748ff92aa27
Kris Poole <krispoole@pm.me>
2025-02-19 15:22:12 -0600

    testing deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,7 @@ try {
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
